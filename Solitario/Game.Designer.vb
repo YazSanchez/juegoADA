8511b5fd1bf54d0c728543024d04126c323f8b9d
YazSanchez <yazmin.sanchez@anima.edu.uy>
2019-08-15 12:40:51 -0300

    Juego

---
+++ Solitario/Game.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Game
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Game))
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ImageList2 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnVerificar = New System.Windows.Forms.Button()
+        Me.lblContador = New System.Windows.Forms.Label()
+        Me.lblMal = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ImageList2
+        '
+        Me.ImageList2.ImageStream = CType(resources.GetObject("ImageList2.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList2.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList2.Images.SetKeyName(0, "1322-steyler-Hotel-1920x960.jpg")
+        Me.ImageList2.Images.SetKeyName(1, "Logo Avion1.png")
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Salmon
+        Me.PictureBox1.Location = New System.Drawing.Point(96, 38)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(173, 242)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Salmon
+        Me.PictureBox2.Location = New System.Drawing.Point(311, 38)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(173, 242)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Salmon
+        Me.PictureBox3.Location = New System.Drawing.Point(96, 296)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(173, 242)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Salmon
+        Me.PictureBox4.Location = New System.Drawing.Point(311, 296)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(173, 242)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(647, 485)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Reset"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnVerificar
+        '
+        Me.btnVerificar.Location = New System.Drawing.Point(647, 436)
+        Me.btnVerificar.Name = "btnVerificar"
+        Me.btnVerificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnVerificar.TabIndex = 5
+        Me.btnVerificar.Text = "Verificar"
+        Me.btnVerificar.UseVisualStyleBackColor = True
+        '
+        'lblContador
+        '
+        Me.lblContador.AutoSize = True
+        Me.lblContador.Location = New System.Drawing.Point(663, 386)
+        Me.lblContador.Name = "lblContador"
+        Me.lblContador.Size = New System.Drawing.Size(0, 13)
+        Me.lblContador.TabIndex = 6
+        '
+        'lblMal
+        '
+        Me.lblMal.AutoSize = True
+        Me.lblMal.Location = New System.Drawing.Point(663, 312)
+        Me.lblMal.Name = "lblMal"
+        Me.lblMal.Size = New System.Drawing.Size(0, 13)
+        Me.lblMal.TabIndex = 7
+        '
+        'Game
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 597)
+        Me.Controls.Add(Me.lblMal)
+        Me.Controls.Add(Me.lblContador)
+        Me.Controls.Add(Me.btnVerificar)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Game"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents ImageList2 As ImageList
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnVerificar As Button
+    Friend WithEvents lblContador As Label
+    Friend WithEvents lblMal As Label
+End Class
---
+++ Solitario/Game.vb
@@ -0,0 +1,137 @@
+﻿Public Class Game
+
+    Dim contador As Integer = 0
+    Dim carta_1 As PictureBox = PictureBox1
+    Dim carta_2 As PictureBox = PictureBox2
+    Dim carta_3 As PictureBox = PictureBox3
+    Dim carta_4 As PictureBox = PictureBox4
+
+    Dim color As Color = Color.Black
+    Dim color2 As Color = Color.Blue
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        PictureBox1.BackColor = color Or color2
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        PictureBox2.BackColor = Color.Pink
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        PictureBox3.BackColor = Color.PaleVioletRed
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        PictureBox4.BackColor = Color.Gray
+
+    End Sub
+
+    Private Sub btnVerificar_Click(sender As Object, e As EventArgs) Handles btnVerificar.Click
+
+        If PictureBox1.BackColor = PictureBox2.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox1.BackColor = PictureBox3.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox1.BackColor = PictureBox4.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+
+        If PictureBox2.BackColor = PictureBox1.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox2.BackColor = PictureBox3.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox2.BackColor = PictureBox4.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox3.BackColor = PictureBox1.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox3.BackColor = PictureBox2.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox3.BackColor = PictureBox4.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox4.BackColor = PictureBox1.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox4.BackColor = PictureBox2.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+        If PictureBox4.BackColor = PictureBox3.BackColor Then
+
+            lblContador.Text = contador + 1
+
+        Else lblMal.Text = "No son iguales"
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.BackColor = Color.Blue
+
+    End Sub
+End Class
